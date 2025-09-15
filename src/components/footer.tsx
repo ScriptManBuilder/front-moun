667963409c219f0e7eb48b13a57884fd709999ae
Dan Hora <dan.gora2004@gmail.com>
2025-09-15 13:09:59 +0300

    Fix: проверка кода перед деплоем

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ import {
   FooterIconLink,
 } from "../assets/styles/footer.style";
 
-import { FaInstagram, FaTelegramPlane } from "react-icons/fa";
-import { SiTiktok } from "react-icons/si";
+import { FaInstagram, FaTelegramPlane, FaLinkedin } from "react-icons/fa";
+import { SiTiktok, SiThreads } from "react-icons/si";
 
 const Footer: React.FC = () => (
   <FooterWrapper>
@@ -16,9 +16,16 @@ const Footer: React.FC = () => (
       <FooterIconLink href="https://www.instagram.com/moun_digital_agency" target="_blank" rel="noopener noreferrer">
         {React.createElement(FaInstagram as any)}
       </FooterIconLink>
+      <FooterIconLink href="https://www.threads.com/@moun_digital_agency" target="_blank" rel="noopener noreferrer">
+        {React.createElement(SiThreads as any)}
+      </FooterIconLink>
+          <FooterIconLink href="https://www.linkedin.com/company/digital-agency-moun/" target="_blank" rel="noopener noreferrer">
+        {React.createElement(FaLinkedin as any)}
+      </FooterIconLink>
       <FooterIconLink href="https://www.tiktok.com/@moun.digital.agen" target="_blank" rel="noopener noreferrer">
         {React.createElement(SiTiktok as any)}
       </FooterIconLink>
+  
       <FooterIconLink href="https://t.me/moun_digital" target="_blank" rel="noopener noreferrer">
         {React.createElement(FaTelegramPlane as any)}
       </FooterIconLink>
